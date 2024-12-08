b2964b4d185876c2501b0a118c4d3a57bff14122
MuhammadSamad30 <romivlogs11@gmail.com>
2024-12-09 00:27:49 +0500

    E-Commerce-Hackathon

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { IoIosArrowForward } from 'react-icons/io'
+import React from 'react';
+import Image from 'next/image'; 
+import Link from 'next/link'; 
+import { IoIosArrowForward } from 'react-icons/io';
 
 const Account = () => {
   return (
@@ -7,18 +9,29 @@ const Account = () => {
       className="relative bg-cover bg-center text-center py-20 px-6"
       style={{
         backgroundImage: "url('/Shop-banner.png')",
-      }}>
-        <img src="/Account-logo.png" alt="Logo" className='flex items-center md:ml-[39%] ml-[4%]' />
+      }}
+    >
+      <div className="flex items-center md:ml-[42%] ml-[18%]">
+        <Image
+          src="/Account-logo.png"
+          alt="Logo"
+          width={200} 
+          height={200} 
+          priority
+        />
+      </div>
 
       <div className="inline-flex items-center">
-        <a href='/' className="text-2xl font-bold">Home 
-        </a>
-        <IoIosArrowForward/>
-        <a href='/account' className="text-2xl font-normal">Account</a>
-        
+        <Link href="/" className="text-2xl font-bold">
+          Home
+        </Link>
+        <IoIosArrowForward />
+        <Link href="/account" className="text-2xl font-normal">
+          Account
+        </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
